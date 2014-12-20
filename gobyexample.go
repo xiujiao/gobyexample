fa365f5076fa008a36a6cbba9264f89e5d07bfa7
XiuJiao Gao <gaoxiujiao@gmail.com>
2014-12-20 15:31:14 -0500

    go values

--- gobyexample.go
+++ gobyexample.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
 	fmt.Println("hello, xiujiao")
+
+	fmt.Println("go" + "long" + "a cool language")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("8.0/2.5=", 8.0/2.5)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+
 }
